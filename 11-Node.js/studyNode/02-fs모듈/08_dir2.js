670558e97a11b01e555b46fcf2e37afab7c834d1
YJH <ybox92@gmail.com>
2025-09-05 17:17:45 +0900

    [Node.js-25-09-05] 비동기식으로 디렉토리 생성 및 삭제 🔥

---
+++ 11-Node.js/studyNode/02-fs모듈/08_dir2.js
@@ -0,0 +1,22 @@
+import fs from "fs"; // FileSystem 모듈 참조
+
+const target = "./docs";
+
+if (!fs.existsSync(target)) {
+	// 파라미터 --> 경로, 퍼미션, 콜백함수
+	fs.mkdir(target, (err) => {
+		if (err) {
+			return console.log(err);
+		}
+		fs.chmodSync(target, "0777");
+		console.log("새로운 docs 폴더를 만들었습니다.");
+	});
+} else {
+	// 파일 삭제 --> 비어있지 않은 폴더는 삭제 못함.
+	fs.rmdir(target, (err) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("docs 폴더를 삭제했습니다.");
+	});
+}
